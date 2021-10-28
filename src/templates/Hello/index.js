98385ea471314dba1f2251f4db86e1b042b72ccb
Maurice Webb <maurice.webb91@gmail.com>
2021-10-28 02:16:21 -0400

    Add  inlineJinja component example

--- src/templates/Hello/index.js
+++ src/templates/Hello/index.js
@@ -2,18 +2,26 @@ import React from 'react'
 import Grid from '../../components/Grid'
 import NumberLink from '../../components/NumberLink'
 import PreHeader from '../../components/PreHeader'
-// import jsonStyles from './styles.json'
 
-// console.log('jsonStyles:', jsonStyles)
+function InlineJinja ({children}) {
+    return (<p>
+        {`{{ `}
+        {children}
+        {` }}`}
+    </p>)
+}
 
 export default function Hello() {
-    console.log(this)
     return (
         <React.Fragment>
             <PreHeader className="pre-header test-extra" text="This is a preheader with hidden text" style={{ color: 'red' }} />
             <Grid>
                 <h1 className="header-class">Hello Ally-Templates 👋🏾</h1>
                 <p className="extra-class">Reach us at: <NumberLink number={18559252559} /></p>
+                <p>{`{{ EventAttribute['promisePaymentDueDate']|dateFormatter('%-m/`}<span>{`%-d`}</span>{`/%Y') }}`}</p>
+                <InlineJinja>
+                    EventAttribute['inlineTestVar'] == true ? <span><strong>yes</strong></span> : <span>no</span>
+                </InlineJinja>
             </Grid>
         </React.Fragment>
     )
